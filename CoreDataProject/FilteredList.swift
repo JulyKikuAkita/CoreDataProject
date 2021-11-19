27b80cc75e38e034d34c6b0b29832b47c6c58ef2
JulyKikuAkita <b92701105@gmail.com>
2021-11-18 23:43:05 -0800

    make FilteredList generic and add ViewBuilder

--- CoreDataProject/FilteredList.swift
+++ CoreDataProject/FilteredList.swift
@@ -6,18 +6,26 @@
 //
 
 import SwiftUI
+import CoreData
+/**
+ @ViewBuilder lets our containing view (whatever is using the list) send in multiple views, and our list will create an implicit HStack just like the regular List.
+ @escaping says the closure will be stored away and used later, which means Swift needs to take care of its memory.
+ */
+struct FilteredList<T: NSManagedObject, Content: View>: View {
+    var fetchRequest: FetchRequest<T>
+    var singers: FetchedResults<T> { fetchRequest.wrappedValue }
 
-struct FilteredList: View {
-    var fetchRequest: FetchRequest<Singer>
-    var singers: FetchedResults<Singer> { fetchRequest.wrappedValue }
+    // this is our content closure; we'll call this once for each item in the list
+    let content: (T) -> Content
 
     var body: some View {
         List(fetchRequest.wrappedValue, id:\.self) { singer in
-            Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+            self.content(singer)
         }
     }
-
-    init(filter: String) {
-        fetchRequest = FetchRequest<Singer>(entity: Singer.entity(), sortDescriptors: [], predicate: NSPredicate(format: "lastName BEGINSWITH %@", filter))
+    
+    init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
+        fetchRequest = FetchRequest<T>(entity: T.entity(), sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+        self.content = content
     }
 }
