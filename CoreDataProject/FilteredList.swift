dd0e83b21e4f24808a86b763cacc83a279afbbc9
JulyKikuAkita <b92701105@gmail.com>
2021-11-18 23:31:49 -0800

    add FilteredList view

---
+++ CoreDataProject/FilteredList.swift
@@ -0,0 +1,23 @@
+//
+//  FilteredList.swift
+//  CoreDataProject
+//
+//  Created by Ifang Lee on 11/18/21.
+//
+
+import SwiftUI
+
+struct FilteredList: View {
+    var fetchRequest: FetchRequest<Singer>
+    var singers: FetchedResults<Singer> { fetchRequest.wrappedValue }
+
+    var body: some View {
+        List(fetchRequest.wrappedValue, id:\.self) { singer in
+            Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+        }
+    }
+
+    init(filter: String) {
+        fetchRequest = FetchRequest<Singer>(entity: Singer.entity(), sortDescriptors: [], predicate: NSPredicate(format: "lastName BEGINSWITH %@", filter))
+    }
+}
---
+++ Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Ifang Lee on 11/18/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
